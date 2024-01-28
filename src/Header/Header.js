1647c52a682d02058fe1883a27b2f969e9e77679
Vinicius Gutierri <viniciusgutierri007@gmail.com>
2024-01-28 13:54:58 -0300

    adicionando projeto

---
+++ src/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './Header.css';
+import smallLeft from '../assets/icons/small-left.png';
+import smallRight from '../assets/icons/small-right.png';
+import search from '../assets/icons/search.png';
+import Playlist from "../Playlist/Playlist";
+
+const Header = () => {
+    return (
+        <div class="main-container">
+            <nav className="header__navigation">
+                <div className="navigation">
+                    <button className="arrow-left">
+                        <img src={smallLeft} alt="Seta esquerda" />
+                    </button>
+                    <button className="arrow-right">
+                        <img src={smallRight} alt="Seta direita" />
+                    </button>
+                </div>
+
+                <div className="header__search">
+                    <img src={search} alt="" />
+                    <input id="search__input" type="text" maxlength="800" placeholder="O que você quer ouvir?"/>
+                </div>
+
+                <div className="header__login">
+                    <button className="subscribe">Inscreva-se</button>
+                    <button className="login">Entrar</button>
+                </div>
+            </nav>
+
+            <Playlist/>
+
+        </div>
+    );
+}
+
+export default Header;
